f61436b535fd85fe615ee189d846dcafc71b29b8
luizrafa-federico <luizrafa.federico@gmail.com>
2021-08-12 09:01:39 -0500

    home page hero ok

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,53 @@
 import Header from "../../components/HeaderDesktop";
 import HeaderMobile from "../../components/HeaderMobile";
-import { HomeContainer } from "./styles";
+import { HomeContainer, HeroContainer, AboutContainer } from "./styles";
+import LottieAnimation from "../../components/Lotties";
+import Animation from "../../assets/HomeAnimation.json";
+import Button from "../../components/Button";
+import Bulb from "../../assets/Bulb.svg";
+import { useHistory } from "react-router-dom";
 
 const Home = () => {
+  const history = useHistory();
+
+  const handleNavigation = (path) => {
+    history.push(path);
+  };
+
   return (
-    <HomeContainer>
+    <>
       <Header />
       <HeaderMobile />
-    </HomeContainer>
+      <HomeContainer>
+        <HeroContainer>
+          <div className="heroInfo">
+            <h1>Construa hábitos</h1>
+            <p>
+              Nosso app vai te ajudar a ter mais disciplina para alcançar os
+              seus objetivos de uma forma rápida e fácil!{" "}
+            </p>
+            <Button onClick={() => handleNavigation("/signup")}>
+              Quero testar!
+            </Button>
+          </div>
+          <div className="lootie">
+            <LottieAnimation lotti={Animation} height={500} width={700} />
+          </div>
+        </HeroContainer>
+        <AboutContainer>
+          <figure>
+            <img src={Bulb} alt="bulb" />
+          </figure>
+          <div className="aboutInfo">
+            <h6>POR QUE EASYHABITS?</h6>
+            <h4>
+              Grandes mudanças começam com pequenos passos. Construa seu novo
+              futuro com EasyHabits
+            </h4>
+          </div>
+        </AboutContainer>
+      </HomeContainer>
+    </>
   );
 };
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,3 +1,63 @@
 import styled from "styled-components";
 
-export const HomeContainer = styled.div``;
+export const HomeContainer = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const HeroContainer = styled.div`
+  display: flex;
+  margin-bottom: 40px;
+
+  .heroInfo {
+    width: 35vw;
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+    margin-top: 40px;
+
+    h1 {
+      color: var(--darkBlue);
+      font-size: 4rem;
+      width: 100px;
+    }
+
+    p {
+      color: var(--darkBlue);
+      width: 450px;
+      font-size: 1.5rem;
+    }
+
+    button {
+      margin-top: 60px;
+      align-self: center;
+      width: 300px;
+      height: 45px;
+    }
+  }
+
+  .lootie {
+    width: 65vw;
+  }
+`;
+
+export const AboutContainer = styled.div`
+  background-color: var(--darkBlue);
+  width: 100vw;
+  min-height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  figure {
+    width: 60vw;
+    height: 300px;
+  }
+
+  .aboutInfo {
+    width: 40vw;
+    height: 300px;
+  }
+`;
