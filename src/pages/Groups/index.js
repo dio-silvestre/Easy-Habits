3ffc8c89371ca7af4a388ef9863c3bd662f055f4
luizrafa-federico <luizrafa.federico@gmail.com>
2021-08-17 11:37:30 -0500

    style: group-form styled

--- src/pages/Groups/index.js
+++ src/pages/Groups/index.js
@@ -1,33 +1,46 @@
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import { useGroups } from "../../Providers/Groups";
+import {
+  PageContainer,
+  FormContainer,
+  OpenGroups,
+  SubscribedGroups,
+} from "./styles";
+import Button from "../../components/Button";
+import TextField from "@material-ui/core/TextField";
 
 const Groups = () => {
   const { register, handleSubmit } = useForm();
   const { groups, subscriptions, addNewGroup } = useGroups();
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(addNewGroup)}>
-        <h1>Entrar no grupo</h1>
-        <section>
-          <input placeholder="Novo Grupo" {...register("name")} name="name" />
-          <input
+    <PageContainer>
+      <FormContainer>
+        <h1>Novo grupo</h1>
+        <h3>Juntos você pode mais!</h3>
+        <form onSubmit={handleSubmit(addNewGroup)}>
+          <TextField
+            placeholder="Novo Grupo"
+            {...register("name")}
+            name="name"
+          />
+          <TextField
             placeholder="Descrição"
             {...register("description")}
             name="description"
             value="Grupo bolado"
           />
-          <input
+          <TextField
             placeholder="Categoria"
             {...register("category")}
             name="category"
             value="CORINGA"
           />
-          <button type="submit">Adicionar</button>
-        </section>
-      </form>
-      <div>
+          <Button type="submit">Adicionar</Button>
+        </form>
+      </FormContainer>
+      <OpenGroups>
         <h1>Grupos Abertos</h1>
         {groups.map((group) => (
           <div key={group.id}>
@@ -36,8 +49,8 @@ const Groups = () => {
             <p>{group.category}</p>
           </div>
         ))}
-      </div>
-      <div>
+      </OpenGroups>
+      <SubscribedGroups>
         <h1>Inscrições</h1>
         {subscriptions.map((group) => (
           <div key={group.id}>
@@ -46,9 +59,9 @@ const Groups = () => {
             <p>{group.category}</p>
           </div>
         ))}
-      </div>
-      <Link to="/dashboard">Hábitos</Link>
-    </div>
+      </SubscribedGroups>
+      {/* <Link to="/dashboard">Hábitos</Link> */}
+    </PageContainer>
   );
 };
 
