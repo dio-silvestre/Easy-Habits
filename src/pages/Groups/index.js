68fa0f0618fd9c247ad85c89a8d238f574f31e12
Nicole Pimenta <niipimenta@hotmail.com>
2021-08-18 14:39:33 -0300

    fix-form-grupos

--- src/pages/Groups/index.js
+++ src/pages/Groups/index.js
@@ -1,4 +1,6 @@
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Link, useHistory } from "react-router-dom";
 import { useGroups } from "../../Providers/Groups";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
@@ -22,9 +24,9 @@ import { useAuth } from "../../Providers/Auth";
 import PersonAddIcon from "@material-ui/icons/PersonAdd";
 import { useState } from "react";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 
 const Groups = () => {
-  const { register, handleSubmit } = useForm();
   const {
     groups,
     subscriptions,
@@ -36,6 +38,20 @@ const Groups = () => {
   const [openForm, setOpenForm] = useState(false);
   const [openGroups, setOpenGroups] = useState(true);
   const history = useHistory();
+
+  const schema = yup.object().shape({
+    name: yup.string().required("Campo obrigatório"),
+    description: yup.string().required("Campo obrigatório"),
+    category: yup.string().required("Campo obrigatório"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const handleForm = (name, description, category) => {
     addNewGroup(name, description, category);
@@ -89,10 +105,18 @@ const Groups = () => {
             <h3>Juntos você pode mais!</h3>
             <form onSubmit={handleSubmit(handleForm)}>
               <TextField placeholder="Nome do Grupo" {...register("name")} />
+              <div className="error"> {errors.name?.message}</div>
               <TextField placeholder="Objetivo" {...register("description")} />
+              <div className="error"> {errors.description?.message}</div>
               <TextField placeholder="Categoria" {...register("category")} />
+              <div className="error"> {errors.category?.message}</div>
               <Button type="submit">Adicionar</Button>
             </form>
+            <p>
+              <Link to="/">
+                <ArrowBackIcon />
+              </Link>
+            </p>
           </FormContainer>
         )}
       </FormModal>
