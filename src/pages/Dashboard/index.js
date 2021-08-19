b6b35d44c995f1c9b5c5e7f9d67381a040834f43
Vinicius <troyackvinicius@gmail.com>
2021-08-19 00:16:12 -0300

    refactor: novo provider para dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,5 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { useEffect, useState } from "react";
-import api from "../../services/api";
-import { useAuth } from "../../Providers/Auth";
 import Button from "../../components/Button";
 import { CardHabit } from "../../components/Card/styles";
 import HeaderDashboard from "../../components/HeaderDashboard";
@@ -17,17 +15,11 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import TextField from "@material-ui/core/TextField";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import { useHabits } from "../../Providers/Habits";
 
 const Dashboard = () => {
-  const [token] = useState(
-    JSON.parse(localStorage.getItem("Habits:token")) || ""
-  );
-  const { userId } = useAuth();
 
-  const [habits, setHabits] = useState([]);
-  const [finishedHabits, setFinishedHabits] = useState([]);
-  const [openNewHabit, setOpenNewHabit] = useState(false);
-  const [loading, setLoading] = useState(true);
+  const { habits, addNewHabit, handleDelete, finishedHabits, handleUpdate, loading, openNewHabit, setOpenNewHabit } = useHabits();
 
   const schema = yup.object().shape({
     habit: yup.string().required("Campo obrigatório"),
@@ -45,119 +37,6 @@ const Dashboard = () => {
     resolver: yupResolver(schema),
   });
 
-  const loadHabits = () => {
-    api
-      .get("/habits/personal/", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((response) => {
-        setHabits(response.data.filter((habit) => habit.achieved === false));
-        setFinishedHabits(
-          response.data.filter((habit) => habit.achieved === true)
-        );
-        setLoading(false);
-      })
-      .catch((err) => console.log(err));
-  };
-
-  useEffect(() => {
-    loadHabits();
-  });
-
-  const addNewHabit = ({ habit, category, difficulty, frequency }) => {
-    api
-      .post(
-        "/habits/",
-        {
-          title: habit,
-          category: category,
-          difficulty: difficulty,
-          frequency: frequency,
-          achieved: false,
-          how_much_achieved: 0,
-          user: userId,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      )
-      .then((response) => {
-        loadHabits();
-        reset();
-      });
-
-    setOpenNewHabit(false);
-  };
-
-  const handleDelete = ({ id }) => {
-    api.delete(`/habits/${id}/`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-  };
-
-  const handleUpdate = ({ id, frequency, how_much_achieved }) => {
-    let achieved = how_much_achieved;
-
-    if (achieved + 9 >= 100 || achieved + 5 >= 100 || achieved + 13 >= 100) {
-      api.patch(
-        `/habits/${id}/`,
-        {
-          achieved: true,
-          how_much_achieved: 100,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-    } else if (frequency === "5") {
-      api.patch(
-        `/habits/${id}/`,
-        {
-          achieved: false,
-          how_much_achieved: (achieved += 100 / 20),
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-    } else if (frequency === "3") {
-      api.patch(
-        `/habits/${id}/`,
-        {
-          achieved: false,
-          how_much_achieved: Math.ceil((achieved += 100 / 12)),
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-    } else {
-      api.patch(
-        `/habits/${id}/`,
-        {
-          achieved: false,
-          how_much_achieved: Math.ceil((achieved += 100 / 8)),
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-    }
-  };
 
   return (
     <>
