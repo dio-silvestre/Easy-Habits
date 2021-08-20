1e95e8b56bd595d34e10b7cb88b8022496a53ceb
Diogo Nava <diogonsdf@gmail.com>
2021-08-20 10:02:34 -0300

    fix: conflitos de merge

--- src/Providers/Auth/index.js
+++ src/Providers/Auth/index.js
@@ -7,28 +7,6 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const token = localStorage.getItem("Habits:token") || "";
-<<<<<<< HEAD
-  const [userDecode, setUserDecode] = useState(token);
-  const [auth, setAuth] = useState(token);
-  const history = useHistory();
-  const logIn = (data) => {
-    api
-      .post("/sessions/", data)
-      .then((response) => {
-        const token = response.data.access;
-
-        localStorage.clear();
-        localStorage.setItem("Habits:token", JSON.stringify(token));
-        setAuth(token);
-        setUserDecode(jwt_decode(token));
-        return history.push("/dashboard");
-      })
-      .catch((err) => console.log(err));
-  };
-
-  return (
-    <AuthContext.Provider value={{ token: auth, setAuth, logIn, userDecode }}>
-=======
   const [auth, setAuth] = useState(token);
 
   const logIn = (data, history) => {
@@ -68,7 +46,6 @@ export const AuthProvider = ({ children }) => {
     <AuthContext.Provider
       value={{ token: auth, setAuth, userId, logIn, logOut }}
     >
->>>>>>> develop
       {children}
     </AuthContext.Provider>
   );
